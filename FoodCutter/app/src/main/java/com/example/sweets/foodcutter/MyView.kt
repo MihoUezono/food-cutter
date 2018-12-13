29c449933cc73684fd7a288ba72207ddf4ddb8f6
nao836 <naogm836@gmail.com>
2018-12-14 00:51:37 +0900

    [wip] zoom canvas by pinch

--- FoodCutter/app/src/main/java/com/example/sweets/foodcutter/MyView.kt
+++ FoodCutter/app/src/main/java/com/example/sweets/foodcutter/MyView.kt
@@ -5,51 +5,61 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.view.TextureView
-import android.view.View
-import android.view.WindowManager
 import android.util.Log
+import android.view.*
 import kotlin.math.*
 
 //図形描画のクラス
-class MyView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+class MyView(context: Context, attrs: AttributeSet) : View(context, attrs), View.OnTouchListener, ScaleGestureDetector.OnScaleGestureListener {
 
     internal var paint: Paint
     private var partCount: Int = 2
     private var cutMode: Int = 0 // 0:circle, 1:rectangle
     private var viewflg: Boolean = false
+    private var scaleGesture: ScaleGestureDetector
+
+    private var currentWidth : Float
+    private var currentHeight: Float
 
     init {
         paint = Paint()
         viewflg = true
+        scaleGesture = ScaleGestureDetector(getContext(), this)
+        setOnTouchListener(this)
+
+        currentWidth = this.width.toFloat()
+        currentHeight = this.height.toFloat()
+
+        Log.v("init", "$currentWidth, $currentHeight")
     }
 
     override fun onDraw(canvas: Canvas) {
 
-        val layoutWidth: Int = this.width
-        val layoutHeight: Int = this.height
+        var cX: Int = this.width/2
+        var cY: Int = this.height/2
 
         // 背景、透明
         canvas.drawColor(Color.argb(0, 0, 0, 0))
 
         when (cutMode) {
             0 -> {
-                var radius : Float = (layoutWidth * 0.9 / 2).toFloat()
+                Log.v("beforeDraw", "$currentWidth, $currentHeight")
+                var radius : Float = (sqrt(currentWidth * currentWidth + currentHeight * currentHeight) * 0.9 / 2).toFloat()
                 // 円
                 paint.color = Color.argb(255, 68, 255, 255)
                 paint.strokeWidth = 10f
                 paint.isAntiAlias = true
                 paint.style = Paint.Style.STROKE
-                canvas.drawCircle(layoutWidth.toFloat() / 2, layoutHeight.toFloat() / 2, radius, paint)
+                canvas.drawCircle(cX.toFloat(), cY.toFloat(), radius, paint)
 
                 // 線
                 paint.strokeWidth = 10f
                 paint.color = Color.argb(255, 0, 255, 120)
                 var count: Int = 0
                 while (++count <= partCount) {
-                    canvas.drawLine(layoutWidth.toFloat() / 2, layoutHeight.toFloat() / 2,
-                            layoutWidth.toFloat() / 2 + radius * sin(PI * 2 * count / partCount).toFloat(),
-                            layoutHeight.toFloat() / 2 - radius * cos(PI * 2 * count / partCount).toFloat(), paint)
+                    canvas.drawLine(cX.toFloat(), cY.toFloat(),
+                            cX.toFloat() + radius * sin(PI * 2 * count / partCount).toFloat(),
+                            cY.toFloat() - radius * cos(PI * 2 * count / partCount).toFloat(), paint)
                 }
             }
             1 -> {
@@ -73,6 +83,27 @@ class MyView(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 } while (count + 1 <= partCount)
             }
         }
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        return scaleGesture.onTouchEvent(event)
+    }
+
+    override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
+        Log.v("onScaleBegin", detector.currentSpan.toString())
+        return true
+    }
+
+    override fun onScale(detector: ScaleGestureDetector): Boolean {
+        Log.v("onScale", detector.currentSpan.toString())
+        currentWidth = detector.currentSpanX
+        currentHeight = detector.currentSpanY
+        showCanvas(true)
+        return true
+    }
+
+    override fun onScaleEnd(detector: ScaleGestureDetector) {
+        Log.v("onScaleEnd", detector.currentSpan.toString())
     }
 
     fun showCanvas(flg: Boolean) {
